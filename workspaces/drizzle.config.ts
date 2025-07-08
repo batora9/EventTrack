2df3ead892f89815cf4153cf3ea5d4c4c5d8546f
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-07-09 07:04:41 +0900

    chore: DB情報更新

--- workspaces/drizzle.config.ts
+++ workspaces/drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: "f6e64e34-45a4-4143-ac61-fd7e63da1478",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
